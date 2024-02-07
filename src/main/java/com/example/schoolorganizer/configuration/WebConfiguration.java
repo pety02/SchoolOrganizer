66fef28d00cefeeb6fd8e7b8745a1bda0214defa
pety02 <pety02@abv.bg>
2024-02-08 00:40:03 +0200

    WebConfiguration class added

---
+++ src/main/java/com/example/schoolorganizer/configuration/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.schoolorganizer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/signin");
+        registry.addViewController("/error").setViewName("");
+    }
+}
