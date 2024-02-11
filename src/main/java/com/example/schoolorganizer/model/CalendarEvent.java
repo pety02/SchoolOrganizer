75097529d4f3677e15ca496c30de0fad0b7bb3f1
pety02 <pety02@abv.bg>
2024-02-11 21:27:31 +0200

    CalendarEvent entity class commented.

--- src/main/java/com/example/schoolorganizer/model/CalendarEvent.java
+++ src/main/java/com/example/schoolorganizer/model/CalendarEvent.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
+/**
+ * This class describes a calendar event.
+ *
+ * @author Petya Licheva
+ */
 @Entity
 @Table
 public class CalendarEvent {
@@ -21,9 +26,21 @@ public class CalendarEvent {
     @ManyToOne
     private User createdBy;
 
+    /**
+     * Default constructor of CalendarEvent class.
+     */
     public CalendarEvent() {
     }
 
+    /**
+     * General purpose constructor of CalendarEvent class.
+     *
+     * @param title     - the title of the event.
+     * @param startDate - the start date of the event.
+     * @param endDate   - the end date of the event.
+     * @param color     - the color of the calendar badge for the event.
+     * @param createdBy - the creator of the event.
+     */
     public CalendarEvent(String title, LocalDate startDate, LocalDate endDate, String color, User createdBy) {
         setTitle(title);
         setStartDate(startDate);
@@ -32,50 +49,111 @@ public class CalendarEvent {
         setCreatedBy(createdBy);
     }
 
+    /**
+     * Get method for the CalendarEvent id.
+     *
+     * @return the id of the event in the database.
+     */
     public Long getCalendarEventId() {
         return calendarEventId;
     }
 
+    /**
+     * Set method for the CalendarEvent id. Used to set the id of the
+     * CalendarEvent when Hibernate ORM generate it.
+     *
+     * @param calendarEventId - the value of the CalendarEvent id.
+     */
     public void setCalendarEventId(Long calendarEventId) {
         this.calendarEventId = calendarEventId;
     }
 
+    /**
+     * Get method for the CalendarEvent title.
+     *
+     * @return the title of the event.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set method for the CalendarEvent title.
+     *
+     * @param title - the CalendarEvent title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get method for the CalendarEvent start date.
+     *
+     * @return the start date of the event.
+     */
     public LocalDate getStartDate() {
         return startDate;
     }
 
+    /**
+     * Set method for the CalendarEvent start date.
+     *
+     * @param startDate - the start date of the CalendarEvent.
+     */
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * Get method for the CalendarEvent end date.
+     *
+     * @return the end date of the CalendarEvent.
+     */
     public LocalDate getEndDate() {
         return endDate;
     }
 
+    /**
+     * Set method for the CalendarEvent end Date.
+     *
+     * @param endDate - the CalendarEvent end date.
+     */
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
 
+    /**
+     * Get method for the CalendarEvent badge's color.
+     *
+     * @return the CalendarEvent badge's color.
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Set method for the CalendarEvent badge's color.
+     *
+     * @param color - the CalendarEvent badge's color value.
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * Get method for CalendarEvent creator.
+     *
+     * @return the CalendarEvent creator.
+     */
     public User getCreatedBy() {
         return createdBy;
     }
 
+    /**
+     * Set method for the CalendarEvent creator.
+     *
+     * @param createdBy - the CalendarEvent creator.
+     */
     public void setCreatedBy(User createdBy) {
         this.createdBy = createdBy;
     }
