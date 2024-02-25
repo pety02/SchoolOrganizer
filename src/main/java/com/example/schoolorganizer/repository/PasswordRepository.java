b63121f8ed465e980c5e7adcc29b1998e81d5bb1
pety02 <pety02@abv.bg>
2024-02-26 00:00:24 +0200

    PasswordRepository class fully documented.

--- src/main/java/com/example/schoolorganizer/repository/PasswordRepository.java
+++ src/main/java/com/example/schoolorganizer/repository/PasswordRepository.java
@@ -5,9 +5,13 @@ import com.example.schoolorganizer.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * This interface describes PasswordRepository with its functionality.
+ *
+ * @author Petya Licheva
+ */
 @Repository
 public interface PasswordRepository extends JpaRepository<Password, Long> {
     boolean existsByOwner(User owner);
