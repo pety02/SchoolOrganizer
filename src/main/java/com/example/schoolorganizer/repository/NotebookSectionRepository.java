16cf98a93589a4325ac57a3dce257f16c4c5416c
pety02 <pety02@abv.bg>
2024-02-26 00:00:10 +0200

    NotebookSectionRepository class fully documented.

--- src/main/java/com/example/schoolorganizer/repository/NotebookSectionRepository.java
+++ src/main/java/com/example/schoolorganizer/repository/NotebookSectionRepository.java
@@ -6,6 +6,11 @@ import com.example.schoolorganizer.model.NotebookSection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * This interface describes NotebookSectionRepository with its functionality.
+ *
+ * @author Petya Licheva
+ */
 @Repository
 public interface NotebookSectionRepository extends JpaRepository<NotebookSection, Long> {
     List<NotebookSection> findAllByAddedInNotebookNotebookId(Long id);
