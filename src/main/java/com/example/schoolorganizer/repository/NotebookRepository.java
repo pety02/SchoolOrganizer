03a498800fa19ecacef74b432dfa021305095fb4
pety02 <pety02@abv.bg>
2024-02-26 00:00:01 +0200

    NotebookRepository class fully documented.

--- src/main/java/com/example/schoolorganizer/repository/NotebookRepository.java
+++ src/main/java/com/example/schoolorganizer/repository/NotebookRepository.java
@@ -6,6 +6,11 @@ import com.example.schoolorganizer.model.Notebook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * This interface describes NotebookRepository with its functionality.
+ *
+ * @author Petya Licheva
+ */
 @Repository
 public interface NotebookRepository extends JpaRepository<Notebook, Long> {
     List<Notebook> findAllByCreatedByUserId(Long id);
