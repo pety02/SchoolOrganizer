a167e1a1bf768075ee8ccc18fbe71aa30247ec68
pety02 <pety02@abv.bg>
2024-02-25 22:23:32 +0200

    CalendarEventAdapterImpl class fully documented.

--- src/main/java/com/example/schoolorganizer/adapter/Impl/CalendarEventAdapterImpl.java
+++ src/main/java/com/example/schoolorganizer/adapter/Impl/CalendarEventAdapterImpl.java
@@ -9,14 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
+ * This class describes CalendarEventAdapterImpl. A class that
+ * transforms a CalendarEvent to a CalendarEventDTO and vice versa.
  *
+ * @author Petya Licheva
  */
 @Component
 public class CalendarEventAdapterImpl implements IAdapter<CalendarEvent, CalendarEventDTO> {
     private final IAdapter<User, UserDTO> userAdapter;
 
     /**
-     * @param userAdapter
+     * General purpose constructor for CalendarEventAdapterImpl class.
+     *
+     * @param userAdapter an autowired user adapter.
      */
     @Autowired
     public CalendarEventAdapterImpl(IAdapter<User, UserDTO> userAdapter) {
@@ -24,8 +29,10 @@ public class CalendarEventAdapterImpl implements IAdapter<CalendarEvent, Calenda
     }
 
     /**
-     * @param entity
-     * @return
+     * Method that transforms calendar event entity to calendar event DTO.
+     *
+     * @param entity calendar event entity.
+     * @return the calendar event DTO.
      */
     @Override
     public CalendarEventDTO fromEntityToDTO(CalendarEvent entity) {
@@ -43,8 +50,10 @@ public class CalendarEventAdapterImpl implements IAdapter<CalendarEvent, Calenda
     }
 
     /**
-     * @param calendarEventDTO
-     * @return
+     * Method that transforms from calendar event DTO to entity.
+     *
+     * @param calendarEventDTO the calendar event DTO.
+     * @return the calendar event entity.
      */
     @Override
     public CalendarEvent fromDTOToEntity(CalendarEventDTO calendarEventDTO) {
