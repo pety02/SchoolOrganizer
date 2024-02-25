0e35bca02ac5739cc4837504fb36c0a885c3e556
pety02 <pety02@abv.bg>
2024-02-25 23:59:07 +0200

    CalendarEventRepository class fully documented.

--- src/main/java/com/example/schoolorganizer/repository/CalendarEventRepository.java
+++ src/main/java/com/example/schoolorganizer/repository/CalendarEventRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * This interface describes CalendarEventRepository with its functionality.
+ *
+ * @author Petya Licheva
+ */
 @Repository
 public interface CalendarEventRepository extends JpaRepository<CalendarEvent, Long> {
     List<CalendarEvent> findAllByCreatedByUserId(Long id);
