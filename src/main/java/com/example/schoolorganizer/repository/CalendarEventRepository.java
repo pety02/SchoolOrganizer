e00f35c9eb9703ea1ab26f6af86198cdf85fb4dc
pety02 <pety02@abv.bg>
2024-02-25 23:59:45 +0200

    CalendarEventRepository class fully documented.

--- src/main/java/com/example/schoolorganizer/repository/CalendarEventRepository.java
+++ src/main/java/com/example/schoolorganizer/repository/CalendarEventRepository.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import com.example.schoolorganizer.model.CalendarEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
