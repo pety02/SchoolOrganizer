1f0e4bb39696116aa76038703808c6a651b71e7f
pety02 <pety02@abv.bg>
2024-02-29 18:14:48 +0200

    SchoolOrganizerErrorController class fully documented.

--- src/main/java/com/example/schoolorganizer/web/SchoolOrganizerErrorController.java
+++ src/main/java/com/example/schoolorganizer/web/SchoolOrganizerErrorController.java
@@ -5,13 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * This class describes a controller that manages
+ * with viewing a custom error page when its needed.
  *
+ * @author Petya Licheva
  */
 @Controller
 public class SchoolOrganizerErrorController implements ErrorController {
 
     /**
-     * @return
+     * This class shows a custom error page.
+     *
+     * @return a html page via the result of the http request.
      */
     @RequestMapping("/error")
     public String handleError() {
