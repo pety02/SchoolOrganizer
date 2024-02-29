f209374005aa56c323810f8bb325a82955eb2ba8
pety02 <pety02@abv.bg>
2024-02-29 18:14:30 +0200

    SchoolOrganizerApplication class fully documented.

--- src/main/java/com/example/schoolorganizer/SchoolOrganizerApplication.java
+++ src/main/java/com/example/schoolorganizer/SchoolOrganizerApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * This class describes the entry point of the School Organizer application.
  *
+ * @author Petya Licheva
  */
 @SpringBootApplication
 public class SchoolOrganizerApplication {
 
     /**
-     * @param args
+     * This method starts the application.
+     *
+     * @param args the args needed of the application to run.
      */
     public static void main(String[] args) {
         SpringApplication.run(SchoolOrganizerApplication.class, args);
