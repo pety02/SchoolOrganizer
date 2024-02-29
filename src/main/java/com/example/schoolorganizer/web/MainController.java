c8d64131ae17a9ced71e010629eaaed43b1c9086
pety02 <pety02@abv.bg>
2024-02-29 17:49:46 +0200

    MainController class fully documented.

--- src/main/java/com/example/schoolorganizer/web/MainController.java
+++ src/main/java/com/example/schoolorganizer/web/MainController.java
@@ -3,7 +3,6 @@ package com.example.schoolorganizer.web;
 import com.example.schoolorganizer.dto.LoginUserDTO;
 import com.example.schoolorganizer.dto.RegisterUserDTO;
 import com.example.schoolorganizer.dto.UserDTO;
-import com.example.schoolorganizer.model.User;
 import com.example.schoolorganizer.security.UserLoggedInValidator;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -11,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
+ * This class describes a MainController. A class that manage
+ * with viewing main html pages such as - registration form,
+ * login form, home page and logging out of definite user's
+ * personal profile.
  *
+ * @author Petya Licheva
  */
 @Controller
 public class MainController {
+
     /**
-     * @param model
-     * @param session
-     * @return
+     * This method shows the registration form.
+     *
+     * @param model   the model object.
+     * @param session the http session object.
+     * @return a html page via its title.
      */
     @GetMapping("/signup")
     public String getRegistrationForm(Model model, HttpSession session) {
@@ -32,9 +39,11 @@ public class MainController {
     }
 
     /**
-     * @param model
-     * @param session
-     * @return
+     * This method shows the login form.
+     *
+     * @param model   the model object.
+     * @param session the http session object.
+     * @return a html page via its title.
      */
     @GetMapping("/signin")
     public String getLoginForm(Model model, HttpSession session) {
@@ -48,25 +57,32 @@ public class MainController {
     }
 
     /**
-     * @param httpSession
-     * @param model
-     * @return
+     * This method shows the home page after successful login.
+     *
+     * @param httpSession the http session.
+     * @param model       the model.
+     * @return a html page vi its title or redirects to the login
+     * form in case of invalidated http session.
      */
     @GetMapping("/home")
     public String getHomeForm(HttpSession httpSession, Model model) {
-        if (UserLoggedInValidator.hasUserLoggedIn(httpSession)) {
-            UserDTO loggedUser = (UserDTO) httpSession.getAttribute("user");
-            model.addAttribute("user", loggedUser);
-            httpSession.setAttribute("foundEvents", null);
-            return "home";
+        if (!UserLoggedInValidator.hasUserLoggedIn(httpSession)) {
+            return "redirect:/signin";
         }
 
-        return "redirect:/signin";
+        UserDTO loggedUser = (UserDTO) httpSession.getAttribute("user");
+        model.addAttribute("user", loggedUser);
+        httpSession.setAttribute("foundEvents", null);
+        return "home";
     }
 
     /**
-     * @param session
-     * @return
+     * This method invalidates the current  http session
+     * and redirects to the login html page.
+     *
+     * @param session the http session.
+     * @return a html page via redirection and the title
+     * of the html page.
      */
     @GetMapping("/logout")
     public String logout(HttpSession session) {
@@ -75,7 +91,9 @@ public class MainController {
     }
 
     /**
-     * @return
+     * This method return the application's custom html error page.
+     *
+     * @return a html page via its title.
      */
     @GetMapping("/error")
     public String getErrorPage() {
