57723dccbf3ed7d5137ba5b96b03076c564cf502
pety02 <pety02@abv.bg>
2024-02-29 22:06:06 +0200

    LoginUserService updated.

--- src/main/java/com/example/schoolorganizer/service/LoginUserService.java
+++ src/main/java/com/example/schoolorganizer/service/LoginUserService.java
@@ -2,7 +2,6 @@ package com.example.schoolorganizer.service;
 
 import com.example.schoolorganizer.dto.LoginUserDTO;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public interface LoginUserService {
